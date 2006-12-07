272034a6f6e89beba2075927acccffc98ef58e54
wikier <wikier@a39e1f30-dc1f-0410-9106-f83088d8df92>
2006-12-07 15:14:38 +0000

    deleted sha column on db

--- trunk/createdb.py
+++ trunk/createdb.py
@@ -5,13 +5,9 @@ if __name__ == '__main__':
 	from pysqlite2 import dbapi2 as sqlite
 	con = sqlite.connect("foaf.db")
 	cur = con.cursor()
-	cur.execute("create table foafs(uri, sha, date, self)")
+	cur.execute("create table foafs(uri, date, self)")
 	cur.execute("""	
-					insert into foafs(uri, sha, date, self) 
-					values (	'http://www.wikier.org/foaf.rdf#wikier',
-								'd0fd987214f56f70b4c47fb96795f348691f93ab',
-								'20061205',
-								'True'
-					)"""
-				)
+					insert into foafs(uri, date, self) 
+					values ('http://www.wikier.org/foaf.rdf#wikier','20061207','True')
+				""")
 	con.commit()
--- trunk/testPySQLite.py
+++ trunk/testPySQLite.py
@@ -5,5 +5,5 @@ from pysqlitewrapper import PySQLiteWrapper
 
 if __name__ == '__main__':
 	pysqlite = PySQLiteWrapper("foaf.db")
-	pysqlite.query("d0fd987214f56f70b4c47fb96795f348691f93ab")
+	pysqlite.query("http://www.wikier.org/foaf.rdf#wikier")
 	
