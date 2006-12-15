9b02b8bd469c16a92d456057f3db2a5bb2aa8ef9
ivanfrade <ivanfrade@a39e1f30-dc1f-0410-9106-f83088d8df92>
2006-12-15 12:34:38 +0000

    Added doc and close method

--- trunk/tracker.py
+++ trunk/tracker.py
@@ -1,11 +1,31 @@
+"""
+    Interface to handle URIs in crawler process.
+    Implementation has complete control in which
+    URIs visit, follow, ban...
+"""
+
 class Tracker:
     
     def moreUrisToExplore(self):
+        """
+         Return a boolean indicating if there are pending URIs to explore
+        """
         pass
     
     def getNextUri(self):
+        """
+         Following URI
+        """
         pass
     
     def putFriendsUris(self, friends):
+        """
+         Friends of the last visited URI
+        """
         pass
     
+    def close(self):
+        """
+         Save data to resumen process, because crawler wants to finish
+        """
+        pass
