085af57b21a483b5745c677bb3a735eaff743c44
ivanfrade <ivanfrade@a39e1f30-dc1f-0410-9106-f83088d8df92>
2006-12-15 12:37:27 +0000

    Added doc and close method

--- trunk/indexer.py
+++ trunk/indexer.py
@@ -1,4 +1,18 @@
+"""
+ Interface with index operation. Each implementation choose how to handle
+already existing uris
+"""
 class Indexer:
     
     def indexFOAFUri(self, uri):
+        """
+         Index FOAF in URI
+        """
+        pass
+
+    def close(self):
+        """
+         Close indices or save data or do what you need, because crawler wants
+        to finish and go to sleep.
+        """
         pass
