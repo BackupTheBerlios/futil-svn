536e0f017e2c078814c0abdc3f31afa3b61654f6
ivanfrade <ivanfrade@a39e1f30-dc1f-0410-9106-f83088d8df92>
2007-01-02 15:57:41 +0000

    Store uri-sha pairs in database

---
+++ trunk/src/futil/storage/shaManager.py
@@ -0,0 +1,37 @@
+import os
+from pysqlite2 import dbapi2 as sqlite
+
+class ShaManager:
+    
+    def __init__(self, path="shas.db"):
+        self.path = path
+        if (not os.path.exists(self.path)):
+            self.createEmptyDB()
+
+    def createEmptyDB(self):
+        (con, cur) = self.connect()
+        cur.execute("CREATE TABLE shas (uri TEXT, sha VARCHAR(40), PRIMARY KEY(uri, sha) )")
+        con.commit()
+        con.close()
+    
+    def insertUriSha(self, uri, sha):
+        (con, cur) = self.connect()
+        query = """
+                INSERT INTO shas(uri, sha)
+                VALUES ('%s','%s')
+            """ % (uri, sha)
+        cur.execute(query)
+        con.commit()
+        con.close() # FIXME 
+    
+    
+    def searchSha(self, sha):
+        con, cur = self.connect()
+        query = "SELECT uri FROM shas WHERE sha =?"
+        cur.execute(query, (sha,))
+        return cur.fetchmany()
+    
+    def connect(self):
+        connection = sqlite.connect(self.path)
+        cursor = connection.cursor()
+        return (connection, cursor)
---
+++ trunk/test/testShaManager.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python2.4
+# -*- coding: utf8 -*-
+
+import sys
+sys.path.append('./src')
+
+from futil.storage.shaManager import ShaManager
+import unittest
+
+import os
+from pysqlite2 import dbapi2 as sqlite
+TESTDB = "testfoaf.db"
+
+SHA = "012345678901234567890123456789"
+
+class TestShaManager(unittest.TestCase):
+
+    def setUp(self):
+        self.shaManager = ShaManager(TESTDB)
+
+    # Helper method
+    def insertElement(self):
+        self.shaManager.insertUriSha("http://www.wikier.org/foaf.rdf", SHA)
+
+    def testInsertion(self):
+        self.insertElement()
+        self.assertTrue(len(self.shaManager.searchSha(SHA)) > 0)
+
+    def testDuplicateInsertion(self):
+        try:
+            self.insertElement()
+            self.insertElement()
+        except sqlite.IntegrityError:
+            self.assertTrue(True)
+
+    def deleteDB(self):
+        os.remove(TESTDB)
+
+
+
+    def tearDown(self):
+        self.deleteDB()
+
+if __name__ == '__main__':
+    unittest.main()
