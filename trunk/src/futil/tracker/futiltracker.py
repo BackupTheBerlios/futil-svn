dff20c7f4a9fde1a4a5cfbeb7f6a2f96663067d3
wikier <wikier@a39e1f30-dc1f-0410-9106-f83088d8df92>
2007-01-02 17:02:19 +0000

    Futil Tracker skell added

---
+++ trunk/src/futil/tracker/futiltracker.py
@@ -0,0 +1,19 @@
+
+from futil.tracker.tracker import Tracker
+
+class FutilTracker(tracker):
+    
+    def __init__(self):
+        pass
+    
+    def moreUrisToExplore(self):
+        return false
+    
+    def getNextUri(self):
+        return None
+    
+    def putFriendsUris(self):
+        pass
+    
+    def close(self):
+        pass
