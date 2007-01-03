7f622ce37bf2970211676812a7d7a28535f5e0fd
ivanfrade <ivanfrade@a39e1f30-dc1f-0410-9106-f83088d8df92>
2007-01-03 12:30:55 +0000

    Fixed test to foaf

--- trunk/test/testfoaf.py
+++ trunk/test/testfoaf.py
@@ -4,17 +4,18 @@ sys.path.append('./src')
 from futil.foaf.foaf import Foaf
 import unittest
 
+from rdflib.Literal import Literal
+
 class TestFoaf(unittest.TestCase):
 
     def setUp(self):
         self._foaf = Foaf('http://frade.no-ip.info:2080/~ivan/foaf.rdf')
 
     def testCreation(self):
-        self.assertEqual(self._foaf.name, "Ivan Frade")
+        self.assertEqual(self._foaf.name[0], Literal("Ivan Frade"))
         # sha as string
-        self.assertEqual(len(self._foaf.sha), 40)
+        self.assertEqual(len(self._foaf.sha[0]), 40)
 
 
 if __name__ == "__main__":
     unittest.main()
-        
