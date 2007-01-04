fc4ba75b2a0ac354f3ad0ec980f6061a4feea7ec
ivanfrade <ivanfrade@a39e1f30-dc1f-0410-9106-f83088d8df92>
2007-01-04 22:32:50 +0000

    Adding xpath code trying to extract more data

--- trunk/src/futil/foaf/foaf.py
+++ trunk/src/futil/foaf/foaf.py
@@ -12,10 +12,13 @@ import os, sys, foaf
 import xml.sax._exceptions
 import rdflib.exceptions
 
-from futil.utils.logger import FutilLogger
+#from futil.utils.logger import FutilLogger
 
 import socket
 socket.setdefaulttimeout(10)
+
+import libxml2
+
 
 FOAF = rdflib.Namespace("http://xmlns.com/foaf/0.1/")
 RDFS = rdflib.Namespace("http://www.w3.org/2000/01/rdf-schema#")
@@ -23,66 +26,87 @@ RDF = rdflib.Namespace("http://www.w3.org/1999/02/22-rdf-syntax-ns#")
 GEO =rdflib.Namespace("http://www.w3.org/2003/01/geo/wgs84_pos#")
 
 queries = [ ("name", ("?value"), GraphPattern([("?node", FOAF['maker'], "?manfloro"),
-                                               ("?manfloro", FOAF['name'], "?value")])),
+                                               ("?manfloro", FOAF['name'], "?value")]),
+                                            "/RDF:rdf/foaf:Person/foaf:name"),
             ("sha", ("?value"), GraphPattern([("?node", FOAF['maker'], "?manfloro"),
-                                              ("?manfloro", FOAF['mbox_sha1sum'], "?value")])),
+                                              ("?manfloro", FOAF['mbox_sha1sum'], "?value")]), None),
             ("nick", ("?value"), GraphPattern([("?node", FOAF['maker'], "?manfloro"),
-                                               ("?manfloro", FOAF['nick'], "?value")])),
+                                               ("?manfloro", FOAF['nick'], "?value")]), None),
             ("friends", ("?sha1", "?uri"), GraphPattern([("?node", FOAF["mbox_sha1sum"], "?sha1"),
-                                                         ("?node", RDFS["seeAlso"], "?uri")])),
+                                                         ("?node", RDFS["seeAlso"], "?uri")]), None),
             ("geopos", ("?geolat", "?geolong"), GraphPattern([("?node", FOAF['maker'], "?manfloro"),
                                                               ("?manfloro", FOAF['based_near'], "?bn"),
                                                               ("?bn",GEO["lat"],"?geolat"),
-                                                              ("?bn",GEO["long"],"?geolong")]))
+                                                              ("?bn",GEO["long"],"?geolong")]), None)
+]
+
+xpaths = [
+    ("name", )
 ]
 
 
 class ErroneousFoaf(Exception):
     def __init__(self, value):
         self.value = value
-        
+
     def __str__(self):
-        return repr(self.value)     
+        return repr(self.value)
 
 class Foaf:
-  
+
+  def tryXpath(self, uri, xpath):
+    print "Trying xpath: ", xpath
+    if xpath == None:
+        return None
+
+    if uri.startswith('http'):
+        doc = libxml2.parseUri(uri)
+    else:
+        file = open(uri[7:]).read()
+        doc = libxml2.parseDoc(file)
+
+    for result in doc.xpathEval(xpath):
+        print "Result xpath: "#, result
+    return None
 
   def __init__(self, foafUri=None):
-    self.log = FutilLogger()
+##    self.log = FutilLogger()
     try:
         if foafUri == None:
           return None
-        
-        if ( foafUri.startswith('/')):	
+
+        if ( foafUri.startswith('/')):
           foafUri = 'file://' + foafUri
-        
+
         self.uri = foafUri
-        
+
         sparqlGr = sparqlGraph.SPARQLGraph()
         sparqlGr.parse(foafUri)
-            
-        for attr, select, where in queries:
+
+        for attr, select, where, xpath in queries:
           result = sparqlGr.query(select, where)
-          if result == None:
+          if result == None or result == []:
+          #  result = self.tryXpath(foafUri, xpath)
+          #if result == None:
             continue
           setattr(self, attr, result)
     except xml.sax._exceptions.SAXParseException:
         print >> sys.stderr , " BAD XML: ", foafUri
-        self.log.error(" BAD XML: " + foafUri)
+##        self.log.error(" BAD XML: " + foafUri)
         raise ErroneousFoaf(foafUri)
     except rdflib.exceptions.ParserError:
         print >> sys.stderr , " BAD RDF: ", foafUri
-        self.log.error(" BAD RDF: " + foafUri)
+##        self.log.error(" BAD RDF: " + foafUri)
         raise ErroneousFoaf(foafUri)
     except UnicodeEncodeError:
         print >> sys.stderr , "Encoding error in ", foafUri
-        self.log.error("Encoding error in " + foafUri)
+##        self.log.error("Encoding error in " + foafUri)
         raise ErroneousFoaf(foafUri)
-    except:
-        print >> sys.stderr , "Something really strange with ", foafUri
-        self.log.error("Encoding error in " + foafUri)
-        raise ErroneousFoaf(foafUri)
-    
+##    except Exception, e:
+##        print >> sys.stderr , "Something really strange with ", foafUri, str(e)
+####        self.log.error("Encoding error in " + foafUri)
+##        raise ErroneousFoaf(foafUri)
+
   def __str__(self):
     text = ""
     for attr, value in self.__dict__.iteritems():
@@ -92,8 +116,10 @@ class Foaf:
       if isinstance(value, rdflib.Literal) or isinstance(value, basestring) :
         text += value.encode('utf-8') + "\n"
       else:
-        if not isinstance(value[0], tuple):
+        if len(value) > 0 and not isinstance(value[0], tuple):
             text += str([i.encode('utf-8') for i in value]) + "\n"
+        else:
+            print "GRRRR " + attr + " - "+ str(value)
     return text
 
 if __name__ == "__main__":
@@ -104,4 +130,4 @@ if __name__ == "__main__":
   foaf = Foaf('noexiste.org')
   print foaf
 
-  
+
