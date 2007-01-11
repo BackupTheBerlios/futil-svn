8008f272e7dc4f1293f28314f0aae165cbf1aff1
ivanfrade <ivanfrade@a39e1f30-dc1f-0410-9106-f83088d8df92>
2007-01-11 21:15:17 +0000

    If there is more than one name, something wrong: return empty

--- trunk/src/futil/foaf/nameFilter.py
+++ trunk/src/futil/foaf/nameFilter.py
@@ -6,23 +6,26 @@ import rdflib
 import xml
 
 class NameFilter(FoafFilter):
-    
+
     def __init__(self, next=None):
         FoafFilter.__init__(self, next)
-        
+
         FOAF = rdflib.Namespace("http://xmlns.com/foaf/0.1/")
         self.select = ("?value")
         self.where = GraphPattern([("?node", FOAF['maker'], "?manfloro"),
                                     ("?manfloro", FOAF['name'], "?value")])
-    
+
     def tryXPath(self, xmlDom, query):
         nodes = self.evaluateXPath(xmlDom, query)
         return map(lambda x: x.toprettyxml().replace('\n',''), nodes)
-    
+
     def process(self, data, foaf):
         #print "Proccessing ", self.__class__
         result = self.evaluateSparQL(data['graph'], self.select, self.where)
         if result == None or result == []:
-            result = self.tryXPath(data['xmlDom'], "/rdf:RDF/foaf:Person/foaf:name/text()")
+            result = self.tryXPath(data['xmlDom'], "/rdf:RDF/foaf:Person/foaf:name[not(@about)]/text()")
+            if len(result) > 1:
+                # Unable to determine One Name
+                result = []
         foaf['name'] = result
         return foaf
