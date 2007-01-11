7e70433e101dfb320c23501fbd3dc30bb4098e47
ivanfrade <ivanfrade@a39e1f30-dc1f-0410-9106-f83088d8df92>
2007-01-11 14:25:30 +0000

    Friends filter

---
+++ trunk/src/futil/foaf/friendsFilter.py
@@ -0,0 +1,32 @@
+from futil.foaf.foafFilter import FoafFilter
+
+from rdflib.sparql.graphPattern import GraphPattern
+import rdflib
+
+import xml
+
+class FriendsFilter(FoafFilter):
+    
+    def __init__(self, next=None):
+        FoafFilter.__init__(self, next)
+        
+        FOAF = rdflib.Namespace("http://xmlns.com/foaf/0.1/")
+        RDFS = rdflib.Namespace("http://www.w3.org/2000/01/rdf-schema#")
+        RDF = rdflib.Namespace("http://www.w3.org/1999/02/22-rdf-syntax-ns#")
+        self.select = ("?sha1", "?uri")
+        self.where = GraphPattern([("?node", RDFS["seeAlso"], "?uri"),
+                                    ("?node", RDF["type"], FOAF["Person"])])
+        self.optional = GraphPattern([("?node", FOAF["mbox_sha1sum"], "?sha1")])
+    
+    def tryXPath(self, xmlDom, query):
+        nodes = self.evaluateXPath(xmlDom, query)
+        return map(lambda x: x.toprettyxml().replace('\n',''), nodes)
+    
+    def process(self, data, foaf):
+        #print "Proccessing ", self.__class__
+        result = self.evaluateSparQL(data['graph'], self.select, self.where, self.optional)
+        result = [(u or '' ,v or '') for (u,v) in result]
+        if result == None or result == []:
+            result = self.tryXPath(data['xmlDom'], "/rdf:RDF/foaf:Person/foaf:name/text()")
+        foaf['friends'] = result
+        return foaf
