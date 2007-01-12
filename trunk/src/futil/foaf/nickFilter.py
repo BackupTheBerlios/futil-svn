995046eb90cf71fed7633739bcbbbfc1fdf6b4ed
ivanfrade <ivanfrade@a39e1f30-dc1f-0410-9106-f83088d8df92>
2007-01-12 11:38:59 +0000

    remove unused comment

--- trunk/src/futil/foaf/nickFilter.py
+++ trunk/src/futil/foaf/nickFilter.py
@@ -7,7 +7,7 @@ import xml
 
 class NickFilter(FoafFilter):
     """
-     Filter to extract sha of foaf's owner
+     Filter to extract nick of foaf's owner. Set the result in "nick" key.
     """
 
     def __init__(self, next=None):
@@ -18,15 +18,10 @@ class NickFilter(FoafFilter):
         self.where = GraphPattern([("?node", FOAF['maker'], "?manfloro"),
                                     ("?manfloro", FOAF['nick'], "?value")])
 
-##    def tryXPath(self, xmldom):
-##        nodes = self.evaluateXPath(xmldom, '/rdf:RDF/foaf:Person/foaf:mbox_sha1sum/text()')
-##        return map(lambda x: x.toprettyxml().replace('\n',''), nodes)
 
 
     def process(self, data, foaf):
 ##        print "Proccessing ", self.__class__
         result = self.evaluateSparQL(data['graph'], self.select, self.where)
-##        if result == None or result == []:
-##            result = self.tryXPath(data['xmlDom'])
         foaf['nick'] = result
         return foaf
