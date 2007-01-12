07c634784067a7dc6ff26b249c0360f580e3433e
ivanfrade <ivanfrade@a39e1f30-dc1f-0410-9106-f83088d8df92>
2007-01-12 11:36:48 +0000

    give me the nick, darling

---
+++ trunk/src/futil/foaf/nickFilter.py
@@ -0,0 +1,32 @@
+from futil.foaf.foafFilter import FoafFilter
+
+from rdflib.sparql.graphPattern import GraphPattern
+import rdflib
+
+import xml
+
+class NickFilter(FoafFilter):
+    """
+     Filter to extract sha of foaf's owner
+    """
+
+    def __init__(self, next=None):
+        FoafFilter.__init__(self, next)
+    
+        FOAF = rdflib.Namespace("http://xmlns.com/foaf/0.1/")
+        self.select = ("?value")
+        self.where = GraphPattern([("?node", FOAF['maker'], "?manfloro"),
+                                    ("?manfloro", FOAF['nick'], "?value")])
+
+##    def tryXPath(self, xmldom):
+##        nodes = self.evaluateXPath(xmldom, '/rdf:RDF/foaf:Person/foaf:mbox_sha1sum/text()')
+##        return map(lambda x: x.toprettyxml().replace('\n',''), nodes)
+
+
+    def process(self, data, foaf):
+##        print "Proccessing ", self.__class__
+        result = self.evaluateSparQL(data['graph'], self.select, self.where)
+##        if result == None or result == []:
+##            result = self.tryXPath(data['xmlDom'])
+        foaf['nick'] = result
+        return foaf
