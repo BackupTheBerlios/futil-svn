0e196ca48de47760faf304b037579b4cf6131330
wikier <wikier@a39e1f30-dc1f-0410-9106-f83088d8df92>
2007-01-15 12:10:55 +0000

    added script to make stats of popular sites

---
+++ trunk/scripts/popularSites.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+SITES=('my.opera.com' 'people.tribe.net' 'livejournal.com' 'ecademy.com' 'linkedin.com' 'kwark.org' 'w3.org')
+
+
+echo Popular Sites:
+echo -------------
+
+for site in ${SITES[@]}; do
+    NUMBER=$(cat log/futil.log | grep pinged | grep -c $site)
+    echo  - $site: $NUMBER
+done
