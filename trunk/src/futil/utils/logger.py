f5a2bf8fc86aabf79ee33544bd1155456b32a1ab
wikier <wikier@a39e1f30-dc1f-0410-9106-f83088d8df92>
2007-01-15 15:16:50 +0000

    better logger levels information

--- trunk/src/futil/utils/logger.py
+++ trunk/src/futil/utils/logger.py
@@ -7,7 +7,7 @@ class FutilLogger:
         self.logger = logging.getLogger(name)
         self.clear()
         self.hdlr = logging.FileHandler('log/' + name + '.log')
-        formatter = logging.Formatter('%(asctime)s %(message)s')
+        formatter = logging.Formatter('%(asctime)s %(levelname)s: %(message)s')
         self.hdlr.setFormatter(formatter)
         self.logger.addHandler(self.hdlr)
         self.logger.setLevel(logging.INFO)
@@ -18,12 +18,12 @@ class FutilLogger:
             self.logger.removeHandler(handler)
 
     def info(self, message):
-        self.logger.info('INFO: ' + message)
+        self.logger.info(message)
         
     def error(self, message):
-        self.logger.error('ERROR: ' + message)
+        self.logger.error(message)
         
     def warn(self, message):
-        self.logger.warn('WARN: ' + message)
+        self.logger.warn(message)
 
 
