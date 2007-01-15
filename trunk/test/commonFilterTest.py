1227bf7a0245ff32a850b722f51b5aa823e957c9
ivanfrade <ivanfrade@a39e1f30-dc1f-0410-9106-f83088d8df92>
2007-01-15 22:00:44 +0000

    Livejournal your friends belongs to me

--- trunk/test/commonFilterTest.py
+++ trunk/test/commonFilterTest.py
@@ -1,7 +1,7 @@
 import unittest
 
 class CommonFilterTest(unittest.TestCase):
-    
+
     FRADE = "data/test/frade.rdf"
     WIKIER = "data/test/wikier.rdf"
     TRIBES = "data/test/tribes.rdf"
@@ -9,15 +9,16 @@ class CommonFilterTest(unittest.TestCase):
     OPERA = "data/test/opera2.rdf"
     BADXML = "data/test/badxml.rdf"
     NOGEOPOS = 'data/test/nogeodata.rdf'
-    
+    LIVEJOURNAL = 'data/test/livejournal.rdf'
+
     def assertListEquals(self, obtained, expected):
         self.assertEquals(len(obtained), len(expected), "Different lenght in list of expected and obtained results")
-         
+
         for e in expected:
             if not e in obtained:
                 self.assertTrue(False, "Expected " + str(e) + " not in obtained results")
         self.assertTrue(True)
-    
+
     def diffList(self, list1, list2):
         only1 = [l for l in list1 if not l in list2]
         only2 = [l for l in list2 if not l in list1]
