8095dde03a9a930c97e16e3c0a08520239a9106f
ivanfrade <ivanfrade@a39e1f30-dc1f-0410-9106-f83088d8df92>
2007-01-15 16:28:43 +0000

    Uncommenting tests

--- trunk/test/testApp.py
+++ trunk/test/testApp.py
@@ -25,29 +25,29 @@ class TestClientView(unittest.TestCase):
         self._directory = RAMDirectory()
         self.shaManager = ShaManager(TESTDB)
         indexer = IndexAppService(self._directory, self.shaManager)
-##        indexer.indexFOAFUri(FRADEURI)
-##        indexer.indexFOAFUri(WIKIERURI)
+        indexer.indexFOAFUri(FRADEURI)
+        indexer.indexFOAFUri(WIKIERURI)
         indexer.close()
 
         self._searcher = SearchAppService(self._directory, self.shaManager)
-##
-##    def testSearchByName(self):
-##        r = self._searcher.search("Sergio")
-##        self.assertEqual(len(r), 1)
-##
-##    def testSearchBySha(self):
-##        r = self._searcher.search("84d076726727b596b08198e26ef37e4817353e97")
-##        self.assertEqual(len(r), 1)
-##        self.assertEqual(r[0].name, u'Ivan Frade')
-##
-##    def testSearchByShaNoExists(self):
-##        r = self._searcher.search("0123456789012345678901234567890123456xxx")
-##        self.assertEqual(len(r), 0)
-##
-##    def testSearchByUri(self):
-##        r = self._searcher.search("http://www.wikier.org/foaf.rdf")
-##        self.assertEqual(len(r), 1)
-##        self.assertEqual(r[0].name, u'Sergio Fernández')
+
+    def testSearchByName(self):
+        r = self._searcher.search("Sergio")
+        self.assertEqual(len(r), 1)
+
+    def testSearchBySha(self):
+        r = self._searcher.search("84d076726727b596b08198e26ef37e4817353e97")
+        self.assertEqual(len(r), 1)
+        self.assertEqual(r[0].name, u'Ivan Frade')
+
+    def testSearchByShaNoExists(self):
+        r = self._searcher.search("0123456789012345678901234567890123456xxx")
+        self.assertEqual(len(r), 0)
+
+    def testSearchByUri(self):
+        r = self._searcher.search("http://www.wikier.org/foaf.rdf")
+        self.assertEqual(len(r), 1)
+        self.assertEqual(r[0].name, u'Sergio Fernández')
 
     def testIndexBadUri(self):
         try:
