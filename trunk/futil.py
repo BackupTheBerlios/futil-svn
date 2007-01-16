92e50c4011ad56a773eea81e2b93f89d0277006f
ivanfrade <ivanfrade@a39e1f30-dc1f-0410-9106-f83088d8df92>
2007-01-16 10:22:32 +0000

    Main with signal management

--- trunk/futil.py
+++ trunk/futil.py
@@ -4,21 +4,21 @@
 import sys
 sys.path.append('./src')
 
-from futil.crawler import Crawler
+from futil.crawler.crawler import Crawler
 from futil.tracker.ptswtracker import PTSWTracker
 from futil.index.appfactory import appServiceFactory
 
-import signal, sys
-
-def finale(a, b):
-    crawler.finish()
-    sys.exit(0)
+import signal
 
 if __name__ == "__main__":
     
     tracker = PTSWTracker(pinged='data/initial.xml')
     indexer = appServiceFactory.createIndexService()
     crawler = Crawler(tracker, indexer)  #FIXME
+
+    def finale(a, b):
+        crawler.finish()
+        sys.exit(0)
 
     signal.signal(signal.SIGHUP, finale)
     crawler.start()
