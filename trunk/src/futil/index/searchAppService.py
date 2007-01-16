8331a566ead2638ac8248cb77a99c5b07fb3b551
ivanfrade <ivanfrade@a39e1f30-dc1f-0410-9106-f83088d8df92>
2007-01-16 23:14:52 +0000

    Return foaf if only knows sha and uri

--- trunk/src/futil/index/searchAppService.py
+++ trunk/src/futil/index/searchAppService.py
@@ -8,20 +8,21 @@ from futil.storage.shaManager import ShaManager
 import operator
 
 class SearchAppService:
-    
+
     def __init__(self, directory, shaManager):
         self._directory = directory
         self._searcher = IndexSearcher(self._directory)
         self._shaManager = shaManager
-    
+
     def _prepareSearcher(self):
         # TODO: test index.currentVersion to update searcher,
         # allowing concurrence in search and write
         if self._searcher:
             self._searcher.close()
         self._searcher = IndexSearcher(self._directory)
-    
+
     def search(self, query):
+        print "Searching ", query
         import re
         if  re.match("^[a-f0-9]{40}$", query):
             return self.searchBySHA(query)
@@ -29,28 +30,35 @@ class SearchAppService:
             return self.searchByURI(query)
         else:
             return self.searchByName(query)
-    
+
     def searchByURI(self, query):
         #print "Searching by URI"
         query = "\"" + query + "\""
         parser = QueryParser("uri", KeywordAnalyzer())
         return self._performSearch(parser, query)
-    
+
     def searchByName(self, query):
         #print "Preguntando por nombre"
         parser = QueryParser("name", StandardAnalyzer())
         return self._performSearch(parser, query)
 
     def searchBySHA(self, query):
-        #print "Preguntando por SHA"
+        print "Preguntando por SHA"
         uris = self._shaManager.searchSha(query)
         if uris == None or len(uris) == 0 :
             return []
-        return reduce(operator.concat, map(self.searchByURI, uris))
+
+        def rebuildFoafs(uri):
+            foaf = self.searchByURI(uri)
+            if foaf == []:
+                return [{'sha':query, 'uri':uri}]
+            else:
+                return foaf
+        return reduce(operator.concat, map(rebuildFoafs, uris))
 
     def _performSearch(self, queryParser,query):
         q = queryParser.parse(query)
-        
+
         self._prepareSearcher()
         hits = self._searcher.search(q)
         result = []
