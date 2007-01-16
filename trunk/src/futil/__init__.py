e671c2b3f56f1c750c27c1b1ccee9eacfba93222
wikier <wikier@a39e1f30-dc1f-0410-9106-f83088d8df92>
2007-01-16 09:07:17 +0000

    added meta information

---
+++ trunk/src/futil/__init__.py
@@ -0,0 +1,23 @@
+# SWAML <http://swaml.berlios.de/>
+# Semantic Web Archive of Mailing Lists
+#
+# Copyright (C) 2006-2007 Ivan Frade, Sergio Fdez
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by the
+# Free Software Foundation; either version 2, or (at your option) any later
+# version.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTIBILITY
+# or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+# for more details.
+
+__author__    = ['Ivan Frade <http://www.asturlinux.org/comunidad/blog/frade>',
+                 'Sergio Fdez <http://www.wikier.org/>']
+__copyright__ = 'Copyright 2006-2007 Ivan Frade and Sergio Fdez'
+__license__   = 'GNU General Public License'
+__version__   = '0.0.1'
+__url__       = 'http://futil.berlios.de/'
+__agent__     = 'http://futil.berlios.de/; ivanfrade@users.berlios.de'
+__doap__      = 'http://futil.berlios.de/resources/doap.rdf'
