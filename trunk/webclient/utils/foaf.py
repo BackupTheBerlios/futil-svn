61b03a64f5551b3460132721411432ab0c5eec3e
ivanfrade <ivanfrade@a39e1f30-dc1f-0410-9106-f83088d8df92>
2007-01-18 21:26:00 +0000

    Print empty nodes to unsetted values

--- trunk/webclient/utils/foaf.py
+++ trunk/webclient/utils/foaf.py
@@ -7,21 +7,25 @@ attributes = ('uri', 'name', 'sha', 'nick', 'friends', 'geolat', 'geolong')
 class Foaf:
     def __init__(self, data):
         self.__dict__ = data
-        
+
     def toxml(self):
         doc = getDOMImplementation().createDocument(None, 'person', None)
         person = doc.documentElement
         #person.setAttribute('xmlns', "FIXME")
-        
+
         for attr in attributes:
-            values = self.__dict__[attr]
-            for value in values:
+            try:
+                values = self.__dict__[attr]
+                for value in values:
+                    node = doc.createElement(attr)
+                    node.appendChild(doc.createTextNode(str(value)))
+                    person.appendChild(node)
+            except:
                 node = doc.createElement(attr)
-                node.appendChild(doc.createTextNode(str(value)))
                 person.appendChild(node)
-        
+
         return person
-        
+
     def __str__(self):
         return self.toxml().toprettyxml(indent='  ', newl='\n')
 
@@ -29,8 +33,8 @@ class Foaf:
 def foafs2xml(foafs):
     doc = getDOMImplementation().createDocument(None, 'response', None)
     response = doc.documentElement
-    
+
     for foaf in foafs:
-        response.appendChild(foaf.toxml().cloneNode(True))
-        
+        response.appendChild(Foaf(foaf).toxml().cloneNode(True))
+
     return doc.toprettyxml(indent='  ', newl='\n')
