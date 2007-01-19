6934729e73123ad3cb35772126b094f268859716
wikier <wikier@a39e1f30-dc1f-0410-9106-f83088d8df92>
2007-01-19 12:50:51 +0000

    pool in png format

--- trunk/scripts/pool.sh
+++ trunk/scripts/pool.sh
@@ -3,8 +3,8 @@
 cat log/futil.log | grep "URIs pending to visit" | awk '{print $4}' | cat -n > data/pool.data
 
 gnuplot << EOF
-set terminal postscript eps color enhanced
-set output "data/pool.eps"
+set terminal png 
+set output "data/pool.png"
 set xlabel "URIs visited (x100)"
 set ylabel "URIs pending to visit"
 set grid
@@ -12,5 +12,5 @@ plot "data/pool.data" using 1:2 with lines title "futil pool size"
 save "data/pool.plt"
 EOF
 
-evince data/pool.eps &
+#gqview data/pool.png &
 
