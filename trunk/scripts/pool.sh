dc76d71f952c6c7bc65feed2772bd7a6214fca46
wikier <wikier@a39e1f30-dc1f-0410-9106-f83088d8df92>
2007-01-24 09:08:44 +0000

    bigger graph

--- trunk/scripts/pool.sh
+++ trunk/scripts/pool.sh
@@ -5,6 +5,7 @@ cat log/futil.log | grep "URIs pending to visit" | awk '{print $4}' | cat -n > d
 gnuplot << EOF
 set terminal png 
 set output "data/pool.png"
+set size 1.5, 1.5
 set xlabel "URIs visited (x100)"
 set ylabel "URIs pending to visit"
 set grid
